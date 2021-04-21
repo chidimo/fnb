4a6007fa2252e29ba2bbb33cba3b0f4eed307825
Chidi Orji <orjichidi95@gmail.com>
2021-04-21 14:17:57 +0100

    feat: hide form if already voted

--- voting/forms.py
+++ voting/forms.py
@@ -32,14 +32,27 @@ class VoteForm(forms.Form):
         print('2', second_place)
         print('3', third_place)
 
-        # check if user has already voted
-        
+        if first_place is None:
+            self.add_error('first_place', 'Please select an option.')
+            return
+
+        if second_place is None:
+            self.add_error('second_place', 'Please select an option.')
+            return
+
+        if third_place is None:
+            self.add_error('third_place', 'Please select an option.')
+            return
+
 
         if first_place == second_place:
             self.add_error('second_place', 'Already selected')
+            return
 
         if first_place == third_place:
             self.add_error('third_place', 'Already selected')
+            return
 
         if second_place == third_place:
             self.add_error('third_place', 'Already selected')
+            return
--- voting/views.py
+++ voting/views.py
@@ -7,34 +7,36 @@ from voting.forms import VoteForm
 
 @login_required
 def vote_screen(request):
-    template = 'voting/vote.html'
+    template = "voting/vote.html"
     user = request.user
     person = user.person
 
-    if request.method == 'POST':
-        form = VoteForm(request.POST, user=user)
+    has_voted = False # Vote.objects.filter(voter__user=user).exists()
+
+    if request.method == "POST":
+        form = VoteForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            print('form data', data)
+            print("form data", data)
 
             first = Point.objects.get(points=15)
             second = Point.objects.get(points=10)
             third = Point.objects.get(points=5)
 
-            first_place = data['first_place']
-            second_place = data['second_place']
-            third_place = data['third_place']
+            first_place = data["first_place"]
+            second_place = data["second_place"]
+            third_place = data["third_place"]
 
             votes_cast = [
-                { "point": first, "contestant": first_place, "voter": person},
-                { "point": second, "contestant": second_place, "voter": person},
-                { "point": third, "contestant": third_place, "voter": person},
+                {"point": first, "contestant": first_place, "voter": person},
+                {"point": second, "contestant": second_place, "voter": person},
+                {"point": third, "contestant": third_place, "voter": person},
             ]
 
             for v in votes_cast:
                 Vote.objects.create(**v)
 
-            return redirect('people:leaderboard')
-        return render(request, template, {'form': form})
+            return redirect("people:leaderboard")
+        return render(request, template, {"form": form})
 
-    return render(request, template, {'form': VoteForm()})
+    return render(request, template, {"form": VoteForm(), "has_voted": has_voted})
